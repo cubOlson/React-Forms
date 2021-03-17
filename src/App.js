a777fd2bb9dc82b7eb5038143b95f717e034930b
Savannah Rice <savannah.rice@gmail.com>
2021-03-17 16:23:21 -0600

    Working on form

--- src/App.js
+++ src/App.js
@@ -13,7 +13,27 @@ function App() {
         <form>
           <div>
             <label htmlFor='name'>Name</label>
-            <input type='text' />
+            <input 
+            onChange={(event)=> setName(event.target.value)}
+            type='text' 
+            value={name} 
+            />
+            <label htmlFor="email">Email</label>
+            <input 
+            onChange={(event)=> setEmail(event.target.value)}
+            type="text"
+            value={email}
+            />
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input 
+            onChange={(event)=> setPhoneNumber(event.target.value)}
+            type="tel"
+            value={phoneNumber}
+            />
+            <div>
+              <label htmlFor="staff">Staff</label>
+              
+            </div>
           </div>
         </form>
 
