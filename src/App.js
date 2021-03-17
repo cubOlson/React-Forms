8f10623b26f870970dc80bd99166944fd328e789
cubOlson <cub.olson@gmail.com>
2021-03-17 16:51:52 -0400

    Just in case

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [staff, setStaff] = useState('student');
+  const [bio, setBio] = useState('');
+  const [notifications, setNotifications] = useState()
   return (
     <h1>Hello from App</h1>
   );
